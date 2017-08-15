eea7ef72369d2f8a0797ec6a67430f6b424ec069
Niklas Hofer <niklas+dev@lanpartei.de>
2017-08-15 15:03:32 +0200

    Heroes are created

--- lib/hero_importer.rb
+++ lib/hero_importer.rb
@@ -1,2 +1,36 @@
+require 'net/http'
+
 class HeroImporter
+  def perform
+    json_data.each do |entry|
+      hero! entry
+    end
+  end
+
+private
+
+  def hero!(entry)
+    Hero.create! entry.slice(*%w[
+      id
+      name
+      real_name
+      health
+      armor
+      shield
+    ])
+  end
+
+  def json_data
+    json['data']
+  end
+
+  def json
+    uri = URI(source_uri)
+    raw = Net::HTTP.get(uri)
+    JSON.parse(raw)
+  end
+
+  def source_uri
+    'https://overwatch-api.net/api/v1/hero/'
+  end
 end
--- spec/lib/hero_importer_spec.rb
+++ spec/lib/hero_importer_spec.rb
@@ -1,7 +1,16 @@
+require 'rails_helper'
 require "hero_importer"
 describe HeroImporter do
+  let(:performing) { lambda {
+    VCR.use_cassette 'overwatch_heros_index' do
+      subject.perform
+    end
+  } }
+
   describe "with empty DB" do
-    it "creates 24 new Heroes"
+    it "creates 24 new Heroes" do
+      expect(performing).to change(Hero, :count).from(0).to(24)
+    end
   end
 
   describe "with 3 Heroes already existing" do
